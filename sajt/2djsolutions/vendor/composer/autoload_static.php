05da373ce74a8ef36c947a58e1d4b384e8b42898
Bruzzknock <srbinsane@hotmail.rs>
2018-04-25 01:49:05 +0200

    Update

--- sajt/2djsolutions/vendor/composer/autoload_static.php
+++ sajt/2djsolutions/vendor/composer/autoload_static.php
@@ -121,6 +121,10 @@ class ComposerStaticInit9c2b824287f307268114be5badc778c4
             'Doctrine\\Bundle\\DoctrineCacheBundle\\' => 36,
             'Doctrine\\Bundle\\DoctrineBundle\\' => 31,
         ),
+        'C' => 
+        array (
+            'Composer\\CaBundle\\' => 18,
+        ),
         'A' => 
         array (
             'App\\Tests\\' => 10,
@@ -431,6 +435,10 @@ class ComposerStaticInit9c2b824287f307268114be5badc778c4
         array (
             0 => __DIR__ . '/..' . '/doctrine/doctrine-bundle',
         ),
+        'Composer\\CaBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/ca-bundle/src',
+        ),
         'App\\Tests\\' => 
         array (
             0 => __DIR__ . '/../..' . '/tests',
@@ -447,6 +455,13 @@ class ComposerStaticInit9c2b824287f307268114be5badc778c4
             'Twig_' => 
             array (
                 0 => __DIR__ . '/..' . '/twig/twig/lib',
+            ),
+        ),
+        'S' => 
+        array (
+            'SensioLabs\\Security' => 
+            array (
+                0 => __DIR__ . '/..' . '/sensiolabs/security-checker',
             ),
         ),
         'P' => 
