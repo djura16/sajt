250b2f5478a1e30276e15bf763935ac90a3c2602
Bruzzknock <srbinsane@hotmail.rs>
2018-04-25 00:57:32 +0200

    Symfony
    Prelaz na symfony

---
+++ sajt/waitasecond/application/logs/log-2018-01-29.php
@@ -0,0 +1,161 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-01-29 01:16:37 --> Register funckija
+ERROR - 2018-01-29 01:20:00 --> Register funckija
+ERROR - 2018-01-29 01:20:43 --> Register funckija
+ERROR - 2018-01-29 01:21:20 --> Register funckija
+ERROR - 2018-01-29 01:21:30 --> Register funckija
+ERROR - 2018-01-29 01:22:14 --> Register funckija
+ERROR - 2018-01-29 01:22:58 --> Register funckija
+ERROR - 2018-01-29 01:23:09 --> Register funckija
+ERROR - 2018-01-29 01:23:17 --> Register funckija
+ERROR - 2018-01-29 01:23:23 --> Register funckija
+ERROR - 2018-01-29 01:23:28 --> Register funckija
+ERROR - 2018-01-29 01:23:38 --> Register funckija
+ERROR - 2018-01-29 01:23:39 --> Register funckija
+ERROR - 2018-01-29 01:23:39 --> Register funckija
+ERROR - 2018-01-29 01:23:40 --> Register funckija
+ERROR - 2018-01-29 01:23:40 --> Register funckija
+ERROR - 2018-01-29 01:24:01 --> Register funckija
+ERROR - 2018-01-29 01:24:22 --> Register funckija
+ERROR - 2018-01-29 01:24:57 --> Register funckija
+ERROR - 2018-01-29 01:25:04 --> Register funckija
+ERROR - 2018-01-29 01:25:40 --> Register funckija
+ERROR - 2018-01-29 01:25:44 --> Login funckija
+ERROR - 2018-01-29 01:25:56 --> Login funckija
+ERROR - 2018-01-29 01:25:56 --> Usli smo u model
+ERROR - 2018-01-29 01:25:56 --> kica
+ERROR - 2018-01-29 01:25:56 --> 123
+ERROR - 2018-01-29 01:25:56 --> Kica
+ERROR - 2018-01-29 01:25:56 --> Usli smo u model
+ERROR - 2018-01-29 01:25:56 --> kica
+ERROR - 2018-01-29 01:25:56 --> 123
+ERROR - 2018-01-29 01:25:56 --> Kica
+ERROR - 2018-01-29 01:25:56 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 01:26:28 --> Register funckija
+ERROR - 2018-01-29 01:26:33 --> Login funckija
+ERROR - 2018-01-29 01:26:33 --> Usli smo u model
+ERROR - 2018-01-29 01:26:33 --> kica
+ERROR - 2018-01-29 01:26:33 --> 123
+ERROR - 2018-01-29 01:26:33 --> Kica
+ERROR - 2018-01-29 01:26:33 --> Usli smo u model
+ERROR - 2018-01-29 01:26:33 --> kica
+ERROR - 2018-01-29 01:26:33 --> 123
+ERROR - 2018-01-29 01:26:33 --> Kica
+ERROR - 2018-01-29 01:26:33 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 01:50:13 --> Register funckija
+ERROR - 2018-01-29 01:52:31 --> Register funckija
+ERROR - 2018-01-29 02:15:24 --> Register funckija
+ERROR - 2018-01-29 02:52:56 --> Register funckija
+ERROR - 2018-01-29 02:53:51 --> Login funckija
+ERROR - 2018-01-29 02:53:51 --> Usli smo u model
+ERROR - 2018-01-29 02:53:51 --> kica
+ERROR - 2018-01-29 02:53:51 --> 123
+ERROR - 2018-01-29 02:53:51 --> Kica
+ERROR - 2018-01-29 02:53:51 --> Usli smo u model
+ERROR - 2018-01-29 02:53:51 --> kica
+ERROR - 2018-01-29 02:53:51 --> 123
+ERROR - 2018-01-29 02:53:51 --> Kica
+ERROR - 2018-01-29 02:53:51 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 02:54:20 --> Login funckija
+ERROR - 2018-01-29 02:54:20 --> Usli smo u model
+ERROR - 2018-01-29 02:54:20 --> kica
+ERROR - 2018-01-29 02:54:20 --> 123
+ERROR - 2018-01-29 02:54:20 --> Kica
+ERROR - 2018-01-29 02:54:20 --> Usli smo u model
+ERROR - 2018-01-29 02:54:20 --> kica
+ERROR - 2018-01-29 02:54:20 --> 123
+ERROR - 2018-01-29 02:54:20 --> Kica
+ERROR - 2018-01-29 02:54:20 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 02:55:21 --> Create metod u Controller
+ERROR - 2018-01-29 02:55:21 --> Create funckija
+ERROR - 2018-01-29 02:55:21 --> 2018-01-29
+ERROR - 2018-01-29 02:55:21 --> 2018-02-04
+ERROR - 2018-01-29 02:56:55 --> Create metod u Controller
+ERROR - 2018-01-29 02:56:55 --> Create funckija
+ERROR - 2018-01-29 02:56:55 --> 2018-01-29
+ERROR - 2018-01-29 02:56:55 --> 2018-02-06
+ERROR - 2018-01-29 02:57:17 --> Create metod u Controller
+ERROR - 2018-01-29 02:57:17 --> Create funckija
+ERROR - 2018-01-29 02:57:17 --> 2018-01-29
+ERROR - 2018-01-29 02:57:17 --> 2018-02-15
+ERROR - 2018-01-29 02:58:37 --> Create metod u Controller
+ERROR - 2018-01-29 02:58:37 --> Create funckija
+ERROR - 2018-01-29 02:58:37 --> 2018-01-29
+ERROR - 2018-01-29 02:58:37 --> 2018-02-27
+ERROR - 2018-01-29 03:39:48 --> Create metod u Controller
+ERROR - 2018-01-29 03:40:11 --> Register funckija
+ERROR - 2018-01-29 03:40:47 --> Register funckija
+ERROR - 2018-01-29 03:40:57 --> Login funckija
+ERROR - 2018-01-29 03:40:57 --> Usli smo u model
+ERROR - 2018-01-29 03:40:57 --> kica
+ERROR - 2018-01-29 03:40:57 --> 123
+ERROR - 2018-01-29 03:40:57 --> Kica
+ERROR - 2018-01-29 03:40:57 --> Usli smo u model
+ERROR - 2018-01-29 03:40:57 --> kica
+ERROR - 2018-01-29 03:40:57 --> 123
+ERROR - 2018-01-29 03:40:57 --> Kica
+ERROR - 2018-01-29 03:40:57 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 03:55:35 --> Severity: Parsing Error --> syntax error, unexpected ')', expecting variable (T_VARIABLE) C:\wamp64\www\waitasecond\application\controllers\Auctions.php 14
+ERROR - 2018-01-29 03:56:02 --> Severity: Parsing Error --> syntax error, unexpected 'var' (T_VAR) C:\wamp64\www\waitasecond\application\controllers\Auctions.php 121
+ERROR - 2018-01-29 03:56:04 --> Severity: Parsing Error --> syntax error, unexpected 'var' (T_VAR) C:\wamp64\www\waitasecond\application\controllers\Auctions.php 121
+ERROR - 2018-01-29 03:56:27 --> Create metod u Controller
+ERROR - 2018-01-29 03:56:27 --> Create funckija
+ERROR - 2018-01-29 03:56:27 --> 2018-01-29
+ERROR - 2018-01-29 03:56:27 --> 2019-01-02
+ERROR - 2018-01-29 18:21:56 --> Severity: Notice --> Undefined index: logged_in C:\wamp64\www\waitasecond\application\views\templates\navbar.php 18
+ERROR - 2018-01-29 18:21:56 --> Severity: Notice --> Undefined index: logged_in C:\wamp64\www\waitasecond\application\views\templates\navbar.php 25
+ERROR - 2018-01-29 18:21:56 --> Severity: Notice --> Undefined index: name C:\wamp64\www\waitasecond\application\views\templates\navbar.php 43
+ERROR - 2018-01-29 18:26:05 --> Register funckija
+ERROR - 2018-01-29 18:36:06 --> Login funckija
+ERROR - 2018-01-29 18:36:06 --> Usli smo u model
+ERROR - 2018-01-29 18:36:06 --> kica
+ERROR - 2018-01-29 18:36:06 --> 123
+ERROR - 2018-01-29 18:36:06 --> Kica
+ERROR - 2018-01-29 18:36:06 --> Usli smo u model
+ERROR - 2018-01-29 18:36:06 --> kica
+ERROR - 2018-01-29 18:36:06 --> 123
+ERROR - 2018-01-29 18:36:06 --> Kica
+ERROR - 2018-01-29 18:36:06 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 19:18:09 --> Register funckija
+ERROR - 2018-01-29 19:20:00 --> Register funckija
+ERROR - 2018-01-29 19:20:58 --> Register funckija
+ERROR - 2018-01-29 19:28:04 --> Register funckija
+ERROR - 2018-01-29 19:28:14 --> Register funckija
+ERROR - 2018-01-29 19:28:45 --> Register funckija
+ERROR - 2018-01-29 19:28:53 --> Register funckija
+ERROR - 2018-01-29 19:29:40 --> Register funckija
+ERROR - 2018-01-29 19:30:45 --> Register funckija
+ERROR - 2018-01-29 19:31:05 --> Login funckija
+ERROR - 2018-01-29 19:31:05 --> Usli smo u model
+ERROR - 2018-01-29 19:31:05 --> kica
+ERROR - 2018-01-29 19:31:05 --> 123
+ERROR - 2018-01-29 19:31:05 --> Kica
+ERROR - 2018-01-29 19:31:05 --> Usli smo u model
+ERROR - 2018-01-29 19:31:05 --> kica
+ERROR - 2018-01-29 19:31:05 --> 123
+ERROR - 2018-01-29 19:31:05 --> Kica
+ERROR - 2018-01-29 19:31:05 --> Usli smo u login aukcije, true
+ERROR - 2018-01-29 19:31:11 --> Login funckija
+ERROR - 2018-01-29 19:31:11 --> Usli smo u model
+ERROR - 2018-01-29 19:31:11 --> kicadsa
+ERROR - 2018-01-29 19:31:11 --> 123
+ERROR - 2018-01-29 19:31:11 --> Severity: Notice --> Trying to get property of non-object C:\wamp64\www\waitasecond\application\models\auctions_model.php 53
+ERROR - 2018-01-29 19:31:11 --> 
+ERROR - 2018-01-29 19:31:11 --> Usli smo u login aukcije, false
+ERROR - 2018-01-29 19:43:48 --> Register funckija
+ERROR - 2018-01-29 21:27:59 --> Register funckija
+ERROR - 2018-01-29 21:28:52 --> Register funckija
+ERROR - 2018-01-29 23:31:38 --> Register funckija
+ERROR - 2018-01-29 23:37:33 --> Register funckija
+ERROR - 2018-01-29 23:50:51 --> Register funckija
+ERROR - 2018-01-29 23:50:58 --> Login funckija
+ERROR - 2018-01-29 23:50:58 --> Usli smo u model
+ERROR - 2018-01-29 23:50:58 --> kica
+ERROR - 2018-01-29 23:50:58 --> 123
+ERROR - 2018-01-29 23:50:58 --> Kica
+ERROR - 2018-01-29 23:50:58 --> Usli smo u model
+ERROR - 2018-01-29 23:50:58 --> kica
+ERROR - 2018-01-29 23:50:58 --> 123
+ERROR - 2018-01-29 23:50:58 --> Kica
+ERROR - 2018-01-29 23:50:58 --> Usli smo u login aukcije, true
---
+++ sajt/waitasecond/application/logs/log-2018-01-30.php
@@ -0,0 +1,45 @@
+ERROR - 2018-01-30 02:53:04 --> Create metod u Controller
+ERROR - 2018-01-30 02:53:04 --> AUCTIONS A TITLE JE
+ERROR - 2018-01-30 02:53:04 --> 
+ERROR - 2018-01-30 02:53:04 --> 2018-01-30
+ERROR - 2018-01-30 02:53:04 --> 1970-01-01
+ERROR - 2018-01-30 02:53:04 --> Query error: Column 'title' cannot be null - Invalid query: INSERT INTO `auction` (`title`, `userID`, `img`, `description`, `startPrice`, `reservePrice`, `current`, `duration`) VALUES (NULL, '1', '/waitasecond/assets/images/maglia.png', NULL, NULL, NULL, '2018-01-30', '1970-01-01')
+ERROR - 2018-01-30 02:54:30 --> Create metod u Controller
+ERROR - 2018-01-30 02:54:30 --> AUCTIONS A TITLE JENovo ime
+ERROR - 2018-01-30 02:54:30 --> Novo ime
+ERROR - 2018-01-30 02:54:30 --> 2018-01-30
+ERROR - 2018-01-30 02:54:30 --> 2018-02-02
+ERROR - 2018-01-30 03:06:43 --> Im in
+ERROR - 2018-01-30 03:06:43 --> kica
+ERROR - 2018-01-30 03:06:43 --> 123
+ERROR - 2018-01-30 03:06:43 --> Im in model login
+ERROR - 2018-01-30 03:06:43 --> Im in model login
+ERROR - 2018-01-30 03:06:43 --> Usli smo u login aukcije, true
+ERROR - 2018-01-30 03:06:43 --> Im in
+ERROR - 2018-01-30 03:06:43 --> kica
+ERROR - 2018-01-30 03:06:43 --> 123
+ERROR - 2018-01-30 03:06:43 --> Im in model login
+ERROR - 2018-01-30 03:06:43 --> Im in model login
+ERROR - 2018-01-30 03:06:43 --> Usli smo u login aukcije, true
+ERROR - 2018-01-30 03:08:33 --> Create metod u Controller
+ERROR - 2018-01-30 03:08:33 --> AUCTIONS A TITLE JEdcsa
+ERROR - 2018-01-30 03:08:33 --> dcsa
+ERROR - 2018-01-30 03:08:33 --> 2018-01-30
+ERROR - 2018-01-30 03:08:33 --> 2018-02-02
+ERROR - 2018-01-30 03:08:33 --> Create metod u Controller
+ERROR - 2018-01-30 03:08:33 --> AUCTIONS A TITLE JEdcsa
+ERROR - 2018-01-30 03:08:33 --> dcsa
+ERROR - 2018-01-30 03:08:33 --> 2018-01-30
+ERROR - 2018-01-30 03:08:33 --> 2018-02-02
+ERROR - 2018-01-30 03:11:37 --> Im in
+ERROR - 2018-01-30 03:11:37 --> kica
+ERROR - 2018-01-30 03:11:37 --> 123
+ERROR - 2018-01-30 03:11:37 --> Im in model login
+ERROR - 2018-01-30 03:11:37 --> Im in model login
+ERROR - 2018-01-30 03:11:37 --> Usli smo u login aukcije, true
+ERROR - 2018-01-30 03:13:00 --> Im in
+ERROR - 2018-01-30 03:13:00 --> kica
+ERROR - 2018-01-30 03:13:00 --> 123
+ERROR - 2018-01-30 03:13:00 --> Im in model login
+ERROR - 2018-01-30 03:13:00 --> Im in model login
+ERROR - 2018-01-30 03:13:00 --> Usli smo u login aukcije, true
